fb792a987ddfe6a3506c8100b6c7d30481a55f4d
Tyler M. Neher <tyler.neher@gmail.com>
2020-05-15 19:19:56 -0400

    update

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,14 @@
+// 1. D3 json import
+var importData = d3.json("samples.json", function (data) {
+  console.log(data);
+});
+
+// 2. Horizonatal Bar Chart
+
+// 3. Bubble Chart
+
+// 4. Display sample metadata
+
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// 6. Update all of the plots any time that a new sample is selected.
