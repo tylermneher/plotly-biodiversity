ea8900440472cbea2be0b18b4c05b9204ba1f8fd
Tyler M. Neher <tyler.neher@gmail.com>
2020-05-18 15:05:27 -0400

    2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,12 +1,24 @@
 // 1. D3 json import
-var importData = d3.json("samples.json", function (data) {
+d3.json("samples.json", function (data) {
   console.log(data);
 });
 
 // 2. Horizonatal Bar Chart
 
-// 3. Bubble Chart
+var trace1 = [{
+    type: 'bar',
+    x: 'sample_values',
+    orientation: 'h'
+}];
 
+Plotly.newPlot('bar', trace1);
+
+// 3. Bubble Chart
+var traceBubble = {
+  x: 'otu_ids',
+  y: 'sample_values',
+  mode: 'markers'
+}
 // 4. Display sample metadata
 
 // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
